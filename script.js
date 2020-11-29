95193a06466c4988b2bea8f3fa2ba3e20df93bb5
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-11-29 21:08:10 +0000

    One more JS Slider

--- script.js
+++ script.js
@@ -69,3 +69,25 @@ function init(){
             }
         });
 }
+
+
+// Infinity Slider
+
+let images = document.querySelectorAll('.infinity-slider img');
+let current = 0;
+
+function slider(){
+    for (let i = 0; i < images.length; i++){
+        images[i].classList.add('opacity0');
+    }
+
+    images[current].classList.remove('opacity0');
+    if (current + 1 == images.length){
+        current =0 ;
+    } else {
+        current++;
+    }
+};
+
+document.querySelector('.infinity-slider').onclick = slider;
+
