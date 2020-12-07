371e0d11a45ebf5e90e23a6e11f91824c5cbc9ee
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-12-07 18:47:05 +0000

    Simple Calculator with JS

---
+++ calc.js
@@ -0,0 +1,63 @@
+function plus(){
+    var num1, num2, result;
+
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1 + num2;
+
+    //inner HTML
+    document.getElementById('result').innerHTML = result;
+
+}
+
+function minus(){
+    var num1, num2, result;
+
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1 - num2;
+
+    //inner HTML
+    document.getElementById('result').innerHTML = result;
+
+}
+
+function multiply(){
+    var num1, num2, result;
+
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1 * num2;
+
+    //inner HTML
+    document.getElementById('result').innerHTML = result;
+
+}
+
+function devide(){
+    var num1, num2;
+
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    var result = num1 / num2;
+    
+    //inner HTML
+    document.getElementById('result').innerHTML = result;
+
+}
